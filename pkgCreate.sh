7f64fcc42ed63ae1aedda5669ac5f3da798955ee
chenxudong2020 <872603935@qq.com>
2024-02-01 08:57:36 +0000

    change to chislash

--- pkgCreate.sh
+++ pkgCreate.sh
@@ -9,6 +9,6 @@ mv package.tgz ../
 cd ../
 rm -rf build/
 mkdir -p build/
-tar -cvf "build/overtls-${pkg_version}.spk" --owner=0 --group=0 scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG package.tgz conf/ WIZARD_UIFILES/
+tar -cvf "build/chislash-${pkg_version}.spk" --owner=0 --group=0 scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG package.tgz conf/ WIZARD_UIFILES/
 rm -f package.tgz
 mv .INFO INFO
